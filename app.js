e468e65dac18f7ef6dfb13d1f022bd0358742215
Michael Bernasol <mfbernasol@gmail.com>
2021-08-05 11:09:03 -0500

    add memongooose-encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,13 @@ app.use(express.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true,useUnifiedTopology: true})
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisasecret"
+userSchema.plugin(encrypt,{ secret:secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User",userSchema);
 
